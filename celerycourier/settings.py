2fe713b7ee81c2b9c8d269d24db56321455c7af6
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2024-07-13 23:23:41 +0300

    added nginx setup

--- celerycourier/settings.py
+++ celerycourier/settings.py
@@ -138,9 +138,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# Absolute path to the directory that will hold collected static files.
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+
+# URL prefix for media files served by Django.
+MEDIA_URL = '/media/'
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
